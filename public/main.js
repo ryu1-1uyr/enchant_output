b39b6d0ce0b947fb71e34dd58c3edf144c25c14d
s17001 <s17001@std.it-college.ac.jp>
2018-11-10 21:25:24 +0900

    コードのリファクタリング

--- public/main.js
+++ public/main.js
@@ -26,24 +26,23 @@ const createPuyo = (game) => {
     return puyo;
 };
 
-function createPair (game, map, field) {
+const createPair = (game, map, field) => {
     let pair = new Group();
-    let p0 = createPuyo(game);  // 回る側の操作ぷよ
-    let p1 = createPuyo(game);  // 軸側の操作ぷよ
+    let rotationPuyo = createPuyo(game);  // 回る側の操作ぷよ
+    let axisPuyo = createPuyo(game);  // 軸側の操作ぷよ
     let forms = [[0, -CELL_SIZE], [CELL_SIZE, 0], [0, CELL_SIZE], [-CELL_SIZE, 0]]; // 操作ぷよの形
     let formNum = 0;                    // 操作ぷよの形の番号。フォームナンバ
     /* キー押下カウント */
     let inputRightCount = 0;
     let inputLeftCount = 0;
-    let inputAcount = 0;
     pair.isFall = true;            // 落下中、つまり操作出来る状態かどうか
-    pair.addChild(p0);             // 操作ぷよをシーンに追加
-    pair.addChild(p1);
-    p0.y = -CELL_SIZE;     // 回る側のぷよの初期位置を軸ぷよの一つ上へ
+    pair.addChild(rotationPuyo);             // 操作ぷよをシーンに追加
+    pair.addChild(axisPuyo);
+    rotationPuyo.y = -CELL_SIZE;     // 回る側のぷよの初期位置を軸ぷよの一つ上へ
     pair.moveTo(CELL_SIZE*3, CELL_SIZE); // グループの初期位置を操作ぷよ出現場所へ
     pair.addEventListener("enterframe", function() {
         // フレーム毎の処理
-        /* キー連続押下カウントの更新 */
+        // キー連続押下カウントの更新 闇の処理
         inputRightCount = game.input.right ? inputRightCount+1 : 0;
         inputLeftCount = game.input.left ? inputLeftCount+1 : 0;
         inputACount = game.input.a ? inputACount+1 : 0;
@@ -54,38 +53,38 @@ function createPair (game, map, field) {
             let newY = forms[newFormNum][1];  // 回転先のy
             if (!map.hitTest(this.x+newX, this.y+newY)) { // 回転可能判定
                 formNum = newFormNum;
-                p0.moveTo(newX, newY);
+                rotationPuyo.moveTo(newX, newY);
             }
         }
         /* 横移動 */
         let newX = 0;                   // 横移動先のx
         if (inputRightCount == 1) {
-            newX = formNum==1 ? p0.x+CELL_SIZE : p1.x+CELL_SIZE;
+            newX = formNum==1 ? rotationPuyo.x+CELL_SIZE : axisPuyo.x+CELL_SIZE;
         }
         if (inputLeftCount == 1) {
-            newX = formNum==3 ? p0.x-CELL_SIZE : p1.x-CELL_SIZE;
+            newX = formNum==3 ? rotationPuyo.x-CELL_SIZE : axisPuyo.x-CELL_SIZE;
         }
-        if (!map.hitTest(this.x+newX, this.y+p0.y) && !map.hitTest(this.x+newX, this.y+p1.y)) { // 移動可能判定
+        if (!map.hitTest(this.x+newX, this.y+rotationPuyo.y) && !map.hitTest(this.x+newX, this.y+axisPuyo.y)) { // 移動可能判定
             this.x = this.x + (newX?newX>=0?1:-1:0)*CELL_SIZE;
         }
         /* 落下 */
-        newY = formNum==2 ? p0.y+CELL_SIZE : p1.y+CELL_SIZE;
+        newY = formNum==2 ? rotationPuyo.y+CELL_SIZE : axisPuyo.y+CELL_SIZE;
         let vy = Math.floor(game.input.down ? game.fps/10 : game.fps/1); // 落下速度の設定 (10や1などの数値は何マス毎秒か
         if (game.frame%vy == 0) {
-            if (!map.hitTest(this.x+p0.x, this.y+newY) && !map.hitTest(this.x+p1.x, this.y+newY)) { // 移動可能判定
+            if (!map.hitTest(this.x+rotationPuyo.x, this.y+newY) && !map.hitTest(this.x+axisPuyo.x, this.y+newY)) { // 移動可能判定
                 this.y += CELL_SIZE;
             } else {                    // 着地した場合
                 /* フィールドに操作ぷよを追加 */
-                field[(this.y+p0.y)/CELL_SIZE][(this.x+p0.x)/CELL_SIZE] = p0.frame;
-                field[(this.y+p1.y)/CELL_SIZE][(this.x+p1.x)/CELL_SIZE] = p1.frame;
+                field[(this.y+rotationPuyo.y)/CELL_SIZE][(this.x+rotationPuyo.x)/CELL_SIZE] = rotationPuyo.frame;
+                field[(this.y+axisPuyo.y)/CELL_SIZE][(this.x+axisPuyo.x)/CELL_SIZE] = axisPuyo.frame;
                 pair.isFall = false; // 着地したので落下中フラグをfalseに
             }
         }
     });
     return pair;
-}
+};
 
-function countPuyos (row, col, field) {
+const  countPuyos = (row, col, field) => {
     let c = field[row][col];    // ぷよの色
     let n = 1;                  // 1で初期化しているのは自分もカウントするため。
     field[row][col] = -1; // この場所をチェックした証として一時的に空白に
@@ -102,13 +101,21 @@ function countPuyos (row, col, field) {
  * 隣接されたぷよが同じ色だった場合は再帰呼び出しし、
  * 消していきます。
  */
-function deletePuyos (row, col, field) {
+const  deletePuyos = (row, col, field) => {
     let c = field[row][col];    // ぷよの色
     field[row][col] = -1;               // ぷよを空に
-    if (row-1>=2 && field[row-1][col]==c) deletePuyos(row-1, col, field);
-    if (row+1<=MAX_ROW-2 && field[row+1][col]==c) deletePuyos(row+1, col, field);
-    if (col-1>=1 && field[row][col-1]==c) deletePuyos(row, col-1, field);
-    if (col+1<=MAX_COL-2 && field[row][col+1]==c) deletePuyos(row, col+1, field);
+    if (row-1>=2 && field[row-1][col]==c) {
+        deletePuyos(row-1, col, field)
+    };
+    if (row+1<=MAX_ROW-2 && field[row+1][col]==c) {
+        deletePuyos(row+1, col, field)
+    };
+    if (col-1>=1 && field[row][col-1]==c) {
+        deletePuyos(row, col-1, field)
+    };
+    if (col+1<=MAX_COL-2 && field[row][col+1]==c) {
+        deletePuyos(row, col+1, field)
+    };
 }
 /**
  * 下が空いているぷよを落とした状態にするよう
@@ -117,7 +124,7 @@ function deletePuyos (row, col, field) {
  * @field {Array} フィールドの色情報が格納された二次元配列
  * @return {Number} 落ちたぷよの数
  */
-function freeFall (field) {
+const freeFall = field => {
     let c = 0;                                  // おちたぷよの数
     for (let i=0; i<MAX_COL; i++) {
         let spaces = 0;
@@ -140,7 +147,7 @@ function freeFall (field) {
  *
  * @field {Array} フィールドの色情報が格納された二次元配列
  */
-function chain (field) {
+const chain = field => {
     for (let i=0; i<MAX_ROW; i++) {
         for (let j=0; j<MAX_COL; j++) {
             let n = 0; // つながっているぷよをカウントする変数を初期化
@@ -150,7 +157,7 @@ function chain (field) {
         }
     }
     if (freeFall(field) >= 1) chain(field); // 自由落下したぷよがあった場合は再帰
-}
+};
 
 const makeField = (field) => {
     for (let i=0; i<field.length; i++) {
@@ -198,7 +205,7 @@ const main = () => {
                 map.loadData(field);     // マップの再読み込み
                 if (field[2][3] != -1) { // ゲームオーバー判定
                     game.stop();
-                    console.log("Game Over");
+                    alert("Game Over");
                 } else {
                     /* 操作ぷよを更新、シーンに追加 */
                     pair = createPair(game, map, field);
