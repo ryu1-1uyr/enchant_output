e2ce55ca9926a59d04ad020475264ea1abf5249a
s17001 <s17001@std.it-college.ac.jp>
2018-11-30 12:04:30 +0900

    いぐのあ外した

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.get('/', function(req, res){
 http.listen(3000);
 module.exports = app;
 console.log("server is running at 3000port")
+
+
+//loop { exp = "#{rand(1..9)} * #{rand(1..9)}"; print "#{exp} = "; e = gets.to_i; a = eval(exp); puts(a == e ? "正解" : "不正解、答えは#{a}") }
+
